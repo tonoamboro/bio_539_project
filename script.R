4c243104eb8c0c71ec6620db2cf857adcdc2e531
Tono Amboro <r.tonoamboro@gmail.com>
2018-05-10 00:30:51 -0400

    create the script

---
+++ script.R
@@ -0,0 +1,9 @@
+#install and load the gridExtra and tidyverse library
+install.packages("gridExtra")
+install.packages("ggpubr")
+library(gridExtra)
+library(tidyverse)
+library("ggpubr")
+
+#load the data
+benoa <- read.csv("data/landing_data.csv")
